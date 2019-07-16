4807112e2a22fa7059be569c7dfa5d93a4f89777
kailashchandra <kailash.daata@gmail.com>
2019-07-16 18:54:07 +0530

    4 files modified, 6 added(Completed all changes.Marked as Done)

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,22 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  item = [];
+  items = [];
+
+  constructor(private httpClient: HttpClient) {
+
+  }
 
   addItem(product) {
-    this.item.push(product);
+    this.items.push(product);
   }
 
   giveItems() {
-    return this.item;
+    return this.items;
   }
 
   clearCart() {
-    this.item = [];
-    return this.item;
+    this.items = [];
+    return this.items;
+  }
+
+  getShippingPrices() {
+    return this.httpClient.get('../assets/shipping.json');
   }
 }
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router'
 
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -11,13 +12,17 @@ import { products } from '../products';
 export class ProductDetailsComponent implements OnInit {
 
   product;
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private cartService: CartService) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(param => {
       this.product = products[+param.get('productId')]
-     // console.log(products[param.get('productId')]);
     });
+  }
+
+  addToCart(product) {
+    window.alert('Your Product has been added !');
+    this.cartService.addItem(product);
   }
 
 }
